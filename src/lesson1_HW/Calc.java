bc58c70342ced0eb89741182b85c8ea0eae1a3a5
shushkovaira <shushkovaira@gmail.com>
2022-07-07 11:32:38 +0300

    work with remarks lesson 1_2

--- src/lesson1_HW/Calc.java
+++ src/lesson1_HW/Calc.java
@@ -1,12 +1,19 @@
 package lesson1_HW;
 
-public class  Calc {
-    public static int substractionDouble(int a, int b) {
+public class Calc {
+    public static int substraction(int videmnik1, int videmnik2) {
 
-        int result = a - b;
+        int result = videmnik1 - videmnik2;
 
         return result;
+    }
 
+
+    public static int multiPlay(int mnognik1, int mnognik2) {
+        int result = mnognik1 * mnognik2;
+        return result;
     }
 }
+
+
 
