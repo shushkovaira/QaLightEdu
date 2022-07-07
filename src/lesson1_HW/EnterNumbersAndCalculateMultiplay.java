09d33c9ddb40a46da09966fea4007d04117c903a
shushkovaira <shushkovaira@gmail.com>
2022-07-07 11:33:52 +0300

    work with remarks lesson 1_2

--- src/lesson1_HW/EnterNumbersAndCalculateMultiplay.java
+++
@@ -1,9 +0,0 @@
-package lesson1_HW;
-
-public class EnterNumbersAndCalculateMultiplay {
-    public static int multiPlay(int a,int b){
-        int result;
-        result=a*b;
-        return result;
-    }
-}
