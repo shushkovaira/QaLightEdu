c5426a54f6b5dd0f54e03d8160b5f57b620cef40
shushkovaira <shushkovaira@gmail.com>
2022-07-15 12:28:42 +0300

    Domashka 5

--- src/DZ5/MainClassWorkWithLists.java
+++ src/DZ5/MainClassWorkWithLists.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class MainClassWorkWithLists {
     public static void main(String[] args) {
         ArrayList<Integer> ourListInteger = new ArrayList<>();
-        ArrayList<Integer> revArrayList = new ArrayList<>();
+
         ourListInteger.add(22);
         ourListInteger.add(66);
         ourListInteger.add(33);
@@ -14,48 +14,37 @@ public class MainClassWorkWithLists {
 
         findMaxElement(ourListInteger);
 
-       reverseElementsInList(ourListInteger);
-        //  System.out.println(reverseElementsInList(ourListInteger));
+        reverseElementsInList(ourListInteger);
+
     }
 
     public static int findMaxElement(ArrayList<Integer> ourListInteger) {
-        //    int[] array2 = {1, -1000, 4, 0, -100};
         int ourListIntegerMaxElement = ourListInteger.get(0);
         for (int i = 1; i < ourListInteger.size(); i++) {
             if (ourListInteger.get(i) > ourListInteger.get(0)) {
                 ourListIntegerMaxElement = ourListInteger.get(i);
             }
-
         }
         System.out.println(ourListIntegerMaxElement);
-
-
         {
             return ourListIntegerMaxElement;
-
         }
-
 
     }
 
     public static ArrayList<Integer> reverseElementsInList(ArrayList<Integer> ourListInteger) {
-        ArrayList<Integer> revArrayList = new ArrayList<Integer>();
-        for (int i = ourListInteger.size() - 1; i >= 0; i--) {
+        ArrayList<Integer> revArrayList = new ArrayList<>();
 
-            revArrayList.add(ourListInteger.get(i));
+        for (int i = 0; i < ourListInteger.size(); i++) {
+
+            revArrayList.add(ourListInteger.get(ourListInteger.size() - i - 1));
         }
-        return revArrayList;
-
+        System.out.println(revArrayList);
+        {
+            return revArrayList;
+        }
     }
-     public static ArrayList<Integer> printElements(ArrayList<Integer> ourListInteger) {
-         for (int i = 0; i < ourListInteger.size(); i++) {
-             System.out.print(ourListInteger.get(i) + " ");
-         }
-         {
-             return ourListInteger;
-         }
 
-     }
 }
 
 
